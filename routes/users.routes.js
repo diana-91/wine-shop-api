a48cfb6c6562dd0cb4b4baa94e26c9b8f6827bf5
diana-91 <diana.hervasmoya@gmail.com>
2018-04-01 20:08:07 +0200

    feat: middleware

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users.controller');
-//secure
 
 router.post('/', usersController.createUser);
 router.put('/:id', usersController.editUser);
