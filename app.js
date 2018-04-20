5aa81fba2ef512a2eccefdd64235880eeff89de2
diana-91 <diana.hervasmoya@gmail.com>
2018-04-20 20:02:43 +0200

    access control origin

--- app.js
+++ app.js
@@ -57,6 +57,26 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://diana-91.github.io/wine-shop-web/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
